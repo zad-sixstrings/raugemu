20c379014a69c7f9a17e3845632fd2c5a9c096c1
Zad Sixstrings <toxxiczad@gmail.com>
2024-12-24 13:31:08 +0100

    updated public paths

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
       <h2 class="main-title-sub purple-text">
         You rip 'em, we emulate 'em.&reg;
       </h2>
-      <img src="/src/assets/raugemu-logo.png" alt="Logo" class="logo" />
+      <img src="/assets/raugemu-logo.png" alt="Logo" class="logo" />
     </div>
   </header>
 </template>
