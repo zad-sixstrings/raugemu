96efc982d2eec094f45804dc7cb5298a549b82c8
Zad Sixstrings <toxxiczad@gmail.com>
2024-12-27 18:34:54 +0100

    updated main menu

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -8,21 +8,32 @@
           >
         </li>
         <li>
-          <a class="main-menu-item dropdown" href="#">Consoles</a>
+          <a class="main-menu-item dropdown" href="#">Atari</a>
+          <div class="dropdown-menu">
+            <a class="sub-menu-item" href="#">2600</a>
+            <a class="sub-menu-item" href="#">5200</a>
+            <a class="sub-menu-item" href="#">7800</a>
+            <a class="sub-menu-item" href="#">JAG</a>
+            <a class="sub-menu-item" href="#">LYNX</a>
+          </div>
+        </li>
+        <li>
+          <a class="main-menu-item dropdown" href="#">Nintendo</a>
           <div class="dropdown-menu">
             <a class="sub-menu-item" href="#">NES</a>
             <a class="sub-menu-item" href="#">SNES</a>
             <a class="sub-menu-item" href="#">N64</a>
-            <a class="sub-menu-item" href="#">PS1</a>
-            <a class="sub-menu-item" href="#">A2600</a>
-          </div>
-        </li>
-        <li>
-          <a class="main-menu-item dropdown" href="#">Portables</a>
-          <div class="dropdown-menu">
             <a class="sub-menu-item" href="#">GB</a>
             <a class="sub-menu-item" href="#">GBC</a>
             <a class="sub-menu-item" href="#">GBA</a>
+          </div>
+        </li>
+        <li>
+          <a class="main-menu-item dropdown" href="#">Sega</a>
+          <div class="dropdown-menu">
+            <a class="sub-menu-item" href="#">SMS</a>
+            <a class="sub-menu-item" href="#">SMD</a>
+            <a class="sub-menu-item" href="#">SGG</a>
           </div>
         </li>
         <li><a class="main-menu-item" href="#">Randomizer</a></li>
