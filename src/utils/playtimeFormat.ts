7bb5b815737aae2be2c07d658e463085a8e60c93
Zad Sixstrings <toxxiczad@gmail.com>
2025-01-01 18:49:22 +0100

    fixed playtimeFormat formula

--- src/utils/playtimeFormat.ts
+++ src/utils/playtimeFormat.ts
@@ -1,7 +1,7 @@
 export function playtimeFormat(time: { seconds: number; milliseconds: number }): string {
-    const totalSeconds = time.seconds + time.milliseconds;
-    if (totalSeconds < 60) {
+  const totalSeconds = time.seconds + time.milliseconds / 1000;
+  if (totalSeconds < 60) {
       return `${Math.round(totalSeconds)} secondes`;
-    }
-    return `${Math.round(totalSeconds / 60)} minutes`;
-   }
+  }
+  return `${Math.round(totalSeconds / 60)} minutes`;
+}
