6d4292cab6612615a69085fdba1d073848e0264d
Zad Sixstrings <toxxiczad@gmail.com>
2025-01-02 13:46:44 +0100

    added n64 emulation warning

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,20 @@
         You rip 'em, we emulate 'em.&reg;
       </h2>
       <img src="/assets/raugemu-logo.png" alt="Logo" class="logo" />
+      <div class="header-warning">
+        <span>/!\ L'émulation Nintendo 64 est actuellement éclatax.</span>
+      </div>
     </div>
   </header>
 </template>
+
+<style scoped>
+.header-warning span {
+  position: absolute;
+  color: yellow;
+  font-family: "Pixelify Sans", serif;
+  font-weight: 400;
+  font-size: 0.9em;
+  padding-top: 50px;
+}
+</style>
