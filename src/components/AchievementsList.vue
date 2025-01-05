d8b547525f0febdfd906a91d2342594987a693e9
Zad Sixstrings <toxxiczad@gmail.com>
2025-01-05 23:19:34 +0100

    basic style for achievements

--- src/components/AchievementsList.vue
+++ src/components/AchievementsList.vue
@@ -6,8 +6,8 @@
           <span class="achievement-date">{{ formatDateTime(achievement.unlockdate) }}</span>
         </div>
         <div class="achievement-content">
-          <h5>{{ achievement.achievementname }}</h5>
-          <p>{{ achievement.description }}</p>
+          <h5 class="achievement-name">{{ achievement.achievementname }}</h5>
+          <p class="achievement-description">{{ achievement.description }}</p>
         </div>
       </div>
     </div>
@@ -21,3 +21,47 @@
     achievements: Achievement[];
   }>();
   </script>
+
+<style scoped>
+.achievements-list {
+  display: flex;
+  max-width: 100%;
+  width: 100%;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+
+.achievement-item {
+  width: 200px;
+  height: 150px;
+  padding: 0.8em;
+  background-color: rgba(138, 138, 138, 0.1);
+  overflow: scroll;
+  border-radius: 5px;
+  box-shadow: 1px 1px 5px black;
+}
+
+.achievement-header h4{
+  font-family: var(--font-press-start);
+  font-size: 1rem;
+  color: var(--purple)
+
+}
+
+span.achievement-date {
+  font-family: var(--font-micro);
+  font-size: 1.2em;
+  color: var(--blue);
+}
+
+.achievement-name {
+  font-family: var(--font-pixelify);
+  font-size: 0.8em;
+  font-weight: 700;
+}
+
+.achievement-description {
+  font-family: var(--font-pixelify);
+  font-size: 0.7em;
+}
+</style>
